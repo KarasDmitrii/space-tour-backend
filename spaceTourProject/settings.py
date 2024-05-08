3f63688fa23e7a6e0fa9ca38922ac59ead460ba9
Dmitry Karas <dimakaras83@gmail.com>
2024-05-08 16:00:16 +0400

    change reiquirements.txt & settings

--- spaceTourProject/settings.py
+++ spaceTourProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-wxlv$bd2qe42!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1" , "localhost" , "dmitryKaras.pythonanywhere.com"]
 
 
 # Application definition
